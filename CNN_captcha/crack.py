58778dff8b79337097c0458ceffd5957ba21f6a3
GaoXianming <gao.xposed@gmail.com>
2018-02-14 08:25:56 +0800

    修复bug

--- CNN_captcha/crack.py
+++ CNN_captcha/crack.py
@@ -1,35 +1,29 @@
 import tensorflow as tf
-import cv2
 import numpy as np
 from PIL import Image
 from src.gen_model import create_layer,convert2gray
-from src.config import MAX_CAPTCHA, CHAR_SET_LEN, VALIDATE_STRING
+from src.config import MAX_CAPTCHA, CHAR_SET_LEN, VALIDATE_STRING,IMAGE_HEIGHT,IMAGE_WIDTH
 
-'''
-'''
-#FLAGS=tf.flags.FLAGS
-#tf.flags.DEFINE_string("input","","./out.png")
 
 keep_prob=tf.placeholder(tf.float32)
-X=cv2.imread("1.png")
-print(X)
-#captcha_image = Image.open(X)
-#captcha_source = np.array(captcha_image)
-X=convert2gray(X)
-X=tf.cast(X,tf.float32)
-Y=create_layer(X,keep_prob)
+
+X = tf.placeholder(tf.float32, [None, IMAGE_HEIGHT * IMAGE_WIDTH])
+Y = create_layer(X,keep_prob)
 max_y=tf.argmax(tf.reshape(Y,[MAX_CAPTCHA,CHAR_SET_LEN]),1)
 
-with tf.Session() as sess:
-    saver=tf.train.Saver()
-    #ckpt=tf.train.get_checkpoint_state("./model/break.cpkt-31700")
-    saver.restore(sess,"./model/break.ckpt-31700")
-    rst=sess.run(max_y,feed_dict={keep_prob:1})
-    print(rst)
-    # 把36个数值对应到10个数字与26个字母中
-
-    result=[]
-    for i in range(MAX_CAPTCHA):
-        result.append(VALIDATE_STRING[rst[MAX_CAPTCHA-(MAX_CAPTCHA-i)]])
-    print(result)
+def crick():
+    with tf.Session() as sess:
+        saver=tf.train.Saver()
+        #ckpt=tf.train.get_checkpoint_state("./model/break.cpkt-31700")
+        saver.restore(sess,"./model/break.ckpt-31700")
+        captcha_image = Image.open("./Image/1.png")
+        captcha_source = np.array(captcha_image)
+        image = convert2gray(captcha_source)
+        image = image.flatten() / 255
+        rst=sess.run(max_y,feed_dict={X:[image],keep_prob:1})
+        # 把36个数值对应到10个数字与26个字母中
+        result=[]
+        for i in range(MAX_CAPTCHA):
+            result.append(VALIDATE_STRING[rst[MAX_CAPTCHA-(MAX_CAPTCHA-i)]])
+        print(result)
 
---
+++ README.md
@@ -0,0 +1,41 @@
+# 利用TensorFlow搭建CNN模型识别验证码
+## 一.各文件,目录作用
+1.model/       :存储训练模型
+2.WebSite/     :网页展示测试成果
+2.src/config.py    :配置参数
+3.src/gen_image.py :生成验证码,图片转换
+4.src/gen_model.py :cnn模型
+5.train.py	   :调用cnn模型进行训练
+6.gen_test_image.py :生成并保存一张测试图片
+7.crack.py 	   :对生成图片进行测试
+
+## 二.测试环境
+### 1.硬件,OS
+cpu:Intel Xeon E5 2640 V4 2.4GHZ 单核
+Ram:1G
+OS:ubuntu 16.04 LTS Server
+
+### 2.软件
+pyhon:3.5.2
+#### python包版本
+captcha:0.2.1  
+numpy:1.13.3  
+matplotlib:2.1.1  
+tensorflow:1.4.1  
+pilow:4.0  
+opencv-python:3.4.0.12  
+tensorbaoard:0.4.0rc3  
+ 
+## 三.运行
+1.运行生成模型  
+```
+python3 train.py
+```
+2.生成图片  
+```
+python3 test_iamge_generator.py
+```
+3.对图片进行识别  
+```
+python3 track.py  
+```
